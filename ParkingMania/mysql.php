f5c817670038ef79c766ec0660565949c406bc8d
khalidumar098 <55772460+khalidumar098@users.noreply.github.com>
2020-05-10 14:35:34 -0400

    Update myql.php

--- ParkingMania/mysql.php
+++ ParkingMania/mysql.php
@@ -1,9 +1,9 @@
 <?php
 // connect to the database
-define('DB_USER', '2020S_KEP');
-define('DB_PASSWORD', '2020S_MDB');
-define('DB_HOST', 'imc.kean.edu');
-define('DB_NAME', '2020S_KEP');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+define('DB_HOST', '');
+define('DB_NAME', '');
 
 // Make the connection:
 $db = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die('Could not connect to MySQL: ' . mysqli_connect_error() );
